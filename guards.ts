4221507b93b40bb496c3a0d089a1dbfeb6de638e
VladimirRiz <resin.fhp@gmail.com>
2022-11-02 18:17:45 +0300

    guards

---
+++ guards.ts
@@ -0,0 +1,39 @@
+const strip = (x: string | number): string | number => {
+	if (typeof x === 'number') {
+		return x.toFixed(2);
+	}
+	return x.trim();
+};
+
+class MyResponse {
+	header = 'response header';
+	result: 'response result';
+}
+
+class MyError {
+	header = 'error header';
+	message = 'error message';
+}
+
+function handler(res: MyResponse | MyError) {
+	if (res instanceof MyResponse) {
+		return {
+			info: res.header + res.result,
+		};
+	} else {
+		return {
+			info: res.header + res.message,
+		};
+	}
+}
+
+//==========
+
+type AlertType = 'success' | 'warning' | 'danger';
+
+const setAlertType = (type: AlertType) => {
+	//=====
+};
+
+setAlertType('success');
+// setAlertType('default');
